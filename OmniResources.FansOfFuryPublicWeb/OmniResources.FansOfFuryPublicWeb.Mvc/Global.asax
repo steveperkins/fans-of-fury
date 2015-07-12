1b411d5d22e0ba9147f69259b3ad55c6c3e53d94
Mike Rosack <mrosack@gmail.com>
2015-07-12 13:19:09 -0500

    initial commit of empty web app project for public web

---
+++ OmniResources.FansOfFuryPublicWeb/OmniResources.FansOfFuryPublicWeb.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OmniResources.FansOfFuryPublicWeb.Mvc.MvcApplication" Language="C#" %>
---
+++ OmniResources.FansOfFuryPublicWeb/OmniResources.FansOfFuryPublicWeb.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OmniResources.FansOfFuryPublicWeb.Mvc.Startup))]
+namespace OmniResources.FansOfFuryPublicWeb.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
