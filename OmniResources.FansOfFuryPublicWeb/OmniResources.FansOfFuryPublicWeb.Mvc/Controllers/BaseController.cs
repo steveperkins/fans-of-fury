f2e47c6f794b69b0c64f35a32f1e3e66d12f8c24
Mike Rosack <mrosack@gmail.com>
2015-07-13 18:02:38 -0500

    rip out auth stuff, we're only going to save a single encrypted cookie with auth data, no full scale auth implementation

---
+++ OmniResources.FansOfFuryPublicWeb/OmniResources.FansOfFuryPublicWeb.Mvc/Controllers/BaseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OmniResources.FansOfFuryPublicWeb.Mvc.Controllers
+{
+    public class BaseController : Controller
+    {
+        private const string AuthCookie = "FansOfFuryAuth";
+        private string _authenticatedUserId;
+
+        public string AuthenticatedUserId
+        {
+            get
+            {
+                if (_authenticatedUserId != null)
+                    return _authenticatedUserId;
+
+                var cookie = Request.Cookies[AuthCookie];
+
+                if (cookie == null || string.IsNullOrWhiteSpace(cookie.Value))
+                    return null;
+
+                try
+                {
+                    var cypherBytes = Convert.FromBase64String(cookie.Value);
+                    var clearBytes = MachineKey.Unprotect(cypherBytes);
+                    
+                    if (clearBytes != null)
+                        _authenticatedUserId = Encoding.UTF8.GetString(clearBytes);
+
+                    return _authenticatedUserId;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            set
+            {
+                var clearBytes = Encoding.UTF8.GetBytes(value);
+                var cypherBytes = MachineKey.Protect(clearBytes);
+
+                Response.Cookies.Add(new HttpCookie(AuthCookie)
+                {
+                    Path = "/",
+                    Value = Convert.ToBase64String(cypherBytes)
+                });
+
+                _authenticatedUserId = value;
+            }
+        }
+    }
+}
--- OmniResources.FansOfFuryPublicWeb/OmniResources.FansOfFuryPublicWeb.Mvc/Controllers/QrCodeController.cs
+++ OmniResources.FansOfFuryPublicWeb/OmniResources.FansOfFuryPublicWeb.Mvc/Controllers/QrCodeController.cs
@@ -1,12 +1,9 @@
-﻿using System.Threading.Tasks;
-using System.Web.Mvc;
-using System.Web.Routing;
-using Microsoft.AspNet.Identity;
+﻿using System.Web.Mvc;
 using OmniResources.FansOfFuryPublicWeb.Data.Table.Repository;
 
 namespace OmniResources.FansOfFuryPublicWeb.Mvc.Controllers
 {
-    public class QrCodeController : BaseAuthenticatedController
+    public class QrCodeController : Controller
     {
         private readonly IUserDataRepository _userDataRepo;
 
@@ -15,26 +12,9 @@ namespace OmniResources.FansOfFuryPublicWeb.Mvc.Controllers
             _userDataRepo = userDataRepo;
         }
 
-        public async Task<ActionResult> Index(string id)
+        public ActionResult Index(string id)
         {
-            var authenticatedUserId = User.Identity.GetUserId();
-
-            // If the user is already authenticated, just take them to manage their account
-            if (!string.IsNullOrEmpty(authenticatedUserId))
-                return RedirectToAction("Index", "Manage");
-
-            // If there's no id passed in, there's nothing we can do...
-            if (string.IsNullOrEmpty(id))
-                return RedirectToAction("Index", "Home");
-
-            var user = await _userDataRepo.Get(id);
-
-            // If there's no user yet for this ID, take them to registration
-            if (user == null)
-                return RedirectToAction("Register", "Account", new RouteValueDictionary {{"id", id}});
-
-            // If there is a user for the ID, take them to login
-            return RedirectToAction("Login", "Account");
+            return View();
         }
     }
 }
