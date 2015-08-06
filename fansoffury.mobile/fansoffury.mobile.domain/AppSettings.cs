fe74bf648aa2cb98063043dc8ee1f208f45cf2ca
Brady Potaczek <bpotaczek@yahoo.com>
2015-08-06 10:34:19 -0500

    Changed address for Tony

--- fansoffury.mobile/fansoffury.mobile.domain/AppSettings.cs
+++ fansoffury.mobile/fansoffury.mobile.domain/AppSettings.cs
@@ -5,7 +5,7 @@ namespace fansoffury.mobile
 	public static class AppSettings
 	{
 		//public static string BaseURL = "http://localhost:8080/";
-		public static string BaseURL = "http://192.168.31.68:8080/";
+		public static string BaseURL = "http://192.168.100.101:8080/";
 	}
 }
 
